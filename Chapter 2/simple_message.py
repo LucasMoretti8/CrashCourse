fb2e57e3c99888b32d6ea22ae36e99bfbb386efb
LucasMoretti <lucasmoretti8@gmail.com>
2021-09-13 16:49:25 -0300

    Chapter2+

---
+++ Chapter 2/simple_message.py
@@ -0,0 +1,2 @@
+simple_message = "This is only a simple message"
+print (simple_message)
---
+++ Chapter 2/simple_messages.py
@@ -0,0 +1,5 @@
+simple_messages = "This is only a simple message"
+print (simple_messages)
+
+simple_messages = "But this is another simple message"
+print (simple_messages)
